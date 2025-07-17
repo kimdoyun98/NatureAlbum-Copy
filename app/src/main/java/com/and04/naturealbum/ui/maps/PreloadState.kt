4ef63df2ba6c27bb933cd78a131d2d87b411aace
kimdoyun98 <rladlsxo1250@gmail.com>
2025-07-17 15:43:42 +0900

    [Refactor] 중복 interface 삭제

--- app/src/main/java/com/and04/naturealbum/ui/maps/PreloadState.kt
+++
@@ -1,9 +0,0 @@
-package com.and04.naturealbum.ui.maps
-
-sealed interface PreloadState {
-    data object Loading : PreloadState
-
-    data object Success : PreloadState
-
-    data object Fail : PreloadState
-}
